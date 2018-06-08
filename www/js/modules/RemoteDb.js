542c0b65acef3509bb5aa2efe0bf60d540a0eccd
Engin <ebuyuk@adbtech.com>
2018-06-08 14:40:12 +0300

    remove console log

--- www/js/modules/RemoteDb.js
+++ www/js/modules/RemoteDb.js
@@ -22,7 +22,7 @@ wingsRemoteDbModule.factory('WingsRemoteDbService', ['Restangular','WINGS_CONFIG
      	return Restangular.all(WINGS_CONFIG.INSERT_LOG).post(JSON.stringify(user)+" : "+logText);
      }
     function executeQuery(sql){
-    	console.log('Test')
+    	console.log('Test 2')
     	$http({
   		  method: 'GET',
   		  url: WINGS_CONFIG.WINGS_ASSETS_REMOTE_UPDATE_LINK,
